52ea4180aff6f6e8c4711b4d9ee35b21811c9ee5
Travis Feldkamp <feldkatr@mail.uc.edu>
2023-02-16 17:51:40 -0500

    project init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String partyAffiliation = "w";//declaring variable for user input
+
+
+
+        System.out.println("Enter your political party affiliation");//output to get users answer
+
+        if (partyAffiliation == "D"){//if their party affiliation is D,output this code block
+            System.out.println("You get a Democratic Donkey");//output of D input
+        }
+        else if (partyAffiliation == "R") {//else if their party affiliation is R, output this code block
+            System.out.println("You get a Republican Elephant");//output of R input
+        }
+        else if (partyAffiliation == "I") {//else if their party affiliation is I, output this code block
+            System.out.println("You get an Independent Person");//output of I input
+
+        }
+        else{// else if they do not select D,R,I output this code block
+            System.out.println("You have chosen Other");//output of any other input
+        }
+
     }
 }
